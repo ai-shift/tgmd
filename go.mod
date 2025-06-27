a4268c023c98abd48a982774cb793819ba8cc17a
scipunch <scipunch@gmail.com>
2025-06-27 04:25:34 +0200

    feat: E2E telegramify test

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/ai-shift/tgmd
 
 go 1.24.3
 
-require github.com/gomarkdown/markdown v0.0.0-20250311123330-531bef5e742b
-
-require github.com/yuin/goldmark v1.7.12 // indirect
+require (
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/gomarkdown/markdown v0.0.0-20250311123330-531bef5e742b
+)
--- tgmd_test.go
+++ tgmd_test.go
@@ -6,6 +6,9 @@ import (
 	"slices"
 	"strings"
 	"testing"
+	"strconv"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 func init() {
@@ -23,5 +26,23 @@ func TestRenderTelegram(t *testing.T) {
 			t.Errorf("line %d differs\ngot     : '%s'\nexpected: '%s'\nFull:\n%s", i+1, gotLines[i], expectedLines[i], got)
 			t.Fatal()
 		}
+	}
+}
+
+func TestRenderE2E(t *testing.T) {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("TEST_BOT_TOKEN"))
+	receiverID, _ := strconv.ParseInt(os.Getenv("RECEIVER_PEER_ID"), 10, 64)
+	if err != nil {
+		t.Errorf("failed to create bot API with %s", err)
+		t.Fatal()
+	}
+
+	input, _ := os.ReadFile("_testdata/input1.md")
+	got := Telegramify(string(input))
+	msg := tgbotapi.NewMessage(receiverID, got)
+	msg.ParseMode = "MarkdownV2"
+	_, err = bot.Send(msg)
+	if err != nil {
+		t.Errorf("failed to send telegram messge with %s\nMessage: %s", err, got)
 	}
 }
